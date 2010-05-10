567b7924630608c603fe33fcf79d34ee4ea792e4
jordan <jordan@192.168.1.3>
2010-05-10 19:57:09 -0400

    syntax

--- gis/mysql/points.sql
+++ gis/mysql/points.sql
@@ -2,6 +2,6 @@
 DROP TRIGGER IF EXISTS `geoname_point`;
 CREATE TRIGGER geoname_point BEFORE INSERT ON geoname
     FOR EACH ROW BEGIN
-		SET NEW.point = GeomFromText(CONCAT('POINT(',NEW.latitude, ' ', NEW.longitude, ')'))
-    END
+		SET NEW.point = GeomFromText(CONCAT('POINT(',NEW.latitude, ' ', NEW.longitude, ')'));
+    END;
     
