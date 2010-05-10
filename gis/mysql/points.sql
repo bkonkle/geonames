a46adf52ee29c9b1e2686e6573f30671b7989432
jordan <jordan@192.168.1.3>
2010-05-10 19:50:59 -0400

    delete if exists

--- gis/mysql/points.sql
+++ gis/mysql/points.sql
@@ -1,5 +1,6 @@
 
-CREATE TRIGGER geoname_point_create BEFORE INSERT OR UPDATE ON geoname
+DROP FUNCTION IF EXISTS `geoname_point`;
+CREATE TRIGGER geoname_point BEFORE INSERT OR UPDATE ON geoname
     FOR EACH ROW BEGIN
 		SET NEW.point = GeomFromText(CONCAT('POINT(',NEW.latitude, ' ', NEW.longitude, ')'))
     END
