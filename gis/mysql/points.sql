85af7a6e60294ca5843c3a320eeb6822fce3ec6e
jordan <jordan@192.168.1.3>
2010-05-10 19:55:30 -0400

    can't do insert and update at once, I guess

--- gis/mysql/points.sql
+++ gis/mysql/points.sql
@@ -1,6 +1,6 @@
 
 DROP TRIGGER IF EXISTS `geoname_point`;
-CREATE TRIGGER geoname_point BEFORE INSERT OR UPDATE ON geoname
+CREATE TRIGGER geoname_point BEFORE INSERT ON geoname
     FOR EACH ROW BEGIN
 		SET NEW.point = GeomFromText(CONCAT('POINT(',NEW.latitude, ' ', NEW.longitude, ')'))
     END
